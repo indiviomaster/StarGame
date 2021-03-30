c62fe9aeabbcda5098d29f7253bcca2a4f2c736d
indivioM <indivio@yandex.ru>
2021-03-30 20:42:05 +0300

    Initial commit

---
+++ desktop/src/ru/indivio/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.indivio.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.indivio.StarGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarGame(), config);
+	}
+}
